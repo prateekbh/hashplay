d8f255c0aa5412a0c175bcc724e0465b81da3fd4
Prateek Bhatnagar <prateek.b@flipkart.com>
2015-05-11 23:03:57 +0530

    clearing hashplay repo

--- tags/hp-settings/hp-settings.js
+++
@@ -1,7 +0,0 @@
-riot.tag('hp-settings', '<hp-card><div class="header"> Change your hashtag </div><div class="body"><div class="text"> Set your new hashtag </div><div class="input"><input class="hashtag"></input></div></div><div class="action-panel"><button class="submit" onclick="{saveHashtag}">Save</button></div></hp-card>', function(opts) {
-        this.saveHashtag=function(){
-            var hashValue=this.root.querySelector(".hashtag").value||'';
-            localStorage.setItem("hashTag",hashValue);
-        }
-    
-});
